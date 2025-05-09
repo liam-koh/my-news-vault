67ec0d979bccee29d38709cc117ce39b81d74096
liam-koh <liam@o2pluss.com>
2025-05-09 18:23:28 +0900

    Del: 필요없는 테스트들 제거

--- src/components/atoms/SelectBox/SelectBox.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import SelectBox from './index';
-
-test('renders SelectBox component', () => {
-  render(<SelectBox />);
-});
